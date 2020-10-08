00754bb2a4403512d95c18952a61a9daaff10b9e
sebrandt1 <43499786+sebrandt1@users.noreply.github.com>
2020-10-08 15:07:57 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 using System.Collections.Generic;
 using System.Globalization;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -8,19 +7,27 @@ namespace Assignment_2
 {
     public class Expense
     {
-        public string Name { get; set; }
-        public string Category { get; set; }
-        public decimal Price { get; set; }
-        private static List<Expense> TotalExpenses = new List<Expense>();
-        
-        //Constructor for our Expense class where we set our instance properties
-        public Expense(string name, string category, decimal price)
+        public string Name { get; private set; }
+        public Categories Category { get; private set; }
+        public decimal Price { get; private set; }
+        public static List<Expense> TotalExpenses = new List<Expense>();
+        public enum Categories
         {
-            if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(category) && price > 0)
+            Food,
+            Entertainment,
+            Other,
+            NONE
+        }
+
+        //Constructor for our Expense class where we set our instance properties
+        public Expense(string name, Categories category, decimal price)
+        {
+            if (!string.IsNullOrEmpty(name) && category != Categories.NONE && price > 0)
             {
                 Name = name;
                 Category = category;
                 Price = price;
+                TotalExpenses.Add(this);
                 return;
             }
             Console.WriteLine("One of more values were empty when trying to create expense, try again.");
@@ -30,15 +37,15 @@ namespace Assignment_2
         /// If argument #2 is empty on call, we want to return the total sum of ALL expenses.
         /// If argument #2 contains a specified category we want to return the sum of all expenses of that single category.
         /// </summary>
-        public static decimal SumExpenses(List<Expense> expenses, string category = null)
+        public static decimal SumExpenses(List<Expense> expenses, Categories category = Categories.NONE)
         {
             decimal sum = 0;
 
-            if(category != null)
+            if (category != Categories.NONE)
             {
-                for(int i = 0; i < expenses.Count; i++)
+                for (int i = 0; i < expenses.Count; i++)
                 {
-                    if(expenses[i].Category == category)
+                    if (expenses[i].Category == category)
                     {
                         sum += expenses[i].Price;
                     }
@@ -53,7 +60,7 @@ namespace Assignment_2
             }
             return sum;
         }
-        
+
         /// <summary>
         /// If argument is empty we should clear all expenses from our list.
         /// If argument contains an expense instance we should removed that specified instance alone from our total list.
@@ -95,7 +102,7 @@ namespace Assignment_2
             Console.Write("Price: ");
 
             decimal.TryParse(Console.ReadLine(), out price);
-            
+
             //Since an expense cannot be a negative value (that would be an income) we want to return here and skip the rest of the method if its negative
             if (price < 0)
             {
@@ -103,18 +110,17 @@ namespace Assignment_2
                 return;
             }
 
-            string category = CategoryMenu();
+            Categories category = CategoryMenu();
 
             Console.Clear();
-            Expense exp = new Expense(name, category, price);
-            TotalExpenses.Add(exp);
+            new Expense(name, category, price);
             //Console.WriteLine($"Added expense: {name} with price {price} and category {category}");
         }
 
         /// <summary>
         /// Create and print our Category selection menu and return the selected value as string (so we know what category was selected).
         /// </summary>
-        public static string CategoryMenu()
+        public static Categories CategoryMenu()
         {
             int selected = Program.ShowMenu("Category: ", new string[]
                 {
@@ -123,29 +129,28 @@ namespace Assignment_2
                     "Other"
                 });
 
-            switch(selected)
+            switch (selected)
             {
                 case 0:
-                    return "Food";
+                    return Categories.Food;
 
                 case 1:
-                    return "Entertainment";
+                    return Categories.Entertainment;
 
                 case 2:
-                    return "Other";
+                    return Categories.Other;
 
-                    //There are only 3 possible values since we only input 3 indexes
-                    //But if for some reason some kind of programming voodoo happens we want to default it to something
+                //There are only 3 possible values since we only input 3 indexes
+                //But if for some reason some kind of programming voodoo happens we want to default it to something
                 default:
-                    return "Invalid Option.";
+                    return Categories.NONE;
             }
         }
-        
+
         /// <summary>
         /// If no argument is passed, all expenses will be printed.
         /// </summary>
-        /// public static Expense[] => public static void
-        public static void ShowExpenses(string category = null)
+        public static void ShowExpenses(Categories category = Categories.NONE)
         {
             Console.Clear();
 
@@ -157,7 +162,7 @@ namespace Assignment_2
                 //items.Add($"[{expense.Name}, {expense.Price}, {expense.Category}]");
             }
 
-            if (category != null || TotalExpenses.Count == 0) //if count is = 0 we want to print that there are no expenses
+            if (category != Categories.NONE || TotalExpenses.Count == 0) //if count is = 0 we want to print that there are no expenses
             {                                                 //this will happen after sum of 0 expense objects is calculated below the for loop
                 for (int i = 0; i < TotalExpenses.Count; i++)
                 {
@@ -184,10 +189,17 @@ namespace Assignment_2
         public static void ExpenseMenu()
         {
             Console.Clear();
+            //If total expenses count is less than 1 we want to return here so we dont pass an empty string array into the showmenu which will cause the program to crash
+            if (TotalExpenses.Count < 1)
+            {
+                Console.WriteLine("You have no expenses to remove!");
+                return;
+            }
+
             //Initialize a string array of the same size as our expense array
             string[] items = new string[TotalExpenses.Count];
 
-            for(int i = 0; i < items.Length; i++)
+            for (int i = 0; i < items.Length; i++)
             {
                 //Create a string in the items array at the equivalent index of the expense
                 //So we can use this in our ShowMenu call (since it only accepts string arrays as parameter).
@@ -217,7 +229,7 @@ namespace Assignment_2
                     "[Remove All Expenses]",
                     "[Exit]",
                     });
-                
+
                 switch (selected)
                 {
                     case 0:
@@ -229,7 +241,7 @@ namespace Assignment_2
                         break;
 
                     case 2:
-                        string category = Expense.CategoryMenu();
+                        Expense.Categories category = Expense.CategoryMenu();
                         Expense.ShowExpenses(category);
                         break;
 
@@ -247,7 +259,7 @@ namespace Assignment_2
                         break;
 
                     default:
-                        break;
+                        throw new Exception("Invalid menu option selected.");
                 }
             }
             Console.ReadKey();
@@ -309,16 +321,109 @@ namespace Assignment_2
         }
     }
 
-        [TestClass]
+    [TestClass]
     public class ProgramTests
     {
         [TestMethod]
-        public void ExampleTest()
+        public void SumAllExpenses()
         {
-            FakeConsole console = new FakeConsole("");
-            Program.Main();
-            Assert.AreEqual("Hello!", console.Output);
-            console.Dispose();
+            new Expense("Apple", Expense.Categories.Food, 5.90M);
+            new Expense("Tv", Expense.Categories.Entertainment, 2499.90M);
+            new Expense("Car insurance", Expense.Categories.Other, 1700M);
+            decimal sum = Expense.SumExpenses(Expense.TotalExpenses);
+
+            Assert.AreEqual(4205.80M, sum);
+            Expense.TotalExpenses.Clear();
+        }
+
+        [TestMethod]
+        public void SingleCategory()
+        {
+            new Expense("Apple", Expense.Categories.Food, 5.90M);
+            new Expense("Tv", Expense.Categories.Entertainment, 2499.90M);
+            new Expense("Netflix subscription", Expense.Categories.Entertainment, 159M);
+            new Expense("Car insurance", Expense.Categories.Other, 1700M);
+            Expense.Categories category = Expense.Categories.Entertainment;
+            decimal sum = Expense.SumExpenses(Expense.TotalExpenses, category);
+
+            Assert.AreEqual(2658.90M, sum);
+            Expense.TotalExpenses.Clear();
+        }
+        [TestMethod]
+        public void SingleIncorrectDeclare()
+        {
+            // If price is 0 or lower the product does not get added to the Expense list
+            new Expense("Apple", Expense.Categories.Food, -1700);
+            new Expense("Tv", Expense.Categories.Entertainment, 2499.90M);
+            new Expense("Car insurance", Expense.Categories.Other, 1700M);
+            decimal sum = Expense.SumExpenses(Expense.TotalExpenses);
+
+            Assert.AreEqual(4199.90M, sum);
+            Expense.TotalExpenses.Clear();
+        }
+        [TestMethod]
+        public void ZeroExpenses()
+        {
+            new Expense("T-shirt", Expense.Categories.Other, 0);
+            new Expense("Tv", Expense.Categories.Entertainment, 0);
+            new Expense("Car insurance", Expense.Categories.Other, 0);
+            new Expense("Cellphone", Expense.Categories.Other, 0);
+            decimal sum = Expense.SumExpenses(Expense.TotalExpenses);
+
+            Assert.AreEqual(0, sum);
+            Expense.TotalExpenses.Clear();
+        }
+        [TestMethod]
+        public void AddSameExpense()
+        {
+            Expense.Categories category = Expense.Categories.Other;
+            new Expense("T-shirt", category, 249.90M);
+            new Expense("T-shirt", category, 249.90M);
+            new Expense("Car insurance", category, 1800M);
+            new Expense("Cellphone", category, 4799.90M);
+            decimal sum = Expense.SumExpenses(Expense.TotalExpenses);
+
+            Assert.AreEqual(7099.70M, sum);
+            Expense.TotalExpenses.Clear();
+        }
+        [TestMethod]
+        public void MultipleNegativeValues()
+        {
+            new Expense("Exhaust pipe", Expense.Categories.Other, -999.90M);
+            new Expense("Dirt bike", Expense.Categories.Entertainment, -14900.00M);
+            new Expense("Tentipi Onyx 9", Expense.Categories.Other, -12399.00M);
+            new Expense("Canned soup", Expense.Categories.Food, -49.90M);
+            decimal sum = Expense.SumExpenses(Expense.TotalExpenses);
+
+            Assert.AreEqual(0, sum);
+            Expense.TotalExpenses.Clear();
+        }
+        [TestMethod]
+        public void TestAllCategories()
+        {
+            new Expense("Exhaust pipe", Expense.Categories.Other, 999.90M);
+            new Expense("Dirt bike", Expense.Categories.Entertainment, 14900.00M);
+            new Expense("Tentipi Onyx 9", Expense.Categories.Other, 12399.00M);
+            new Expense("Canned soup", Expense.Categories.Food, 49.90M);
+            new Expense("Canned tuna", Expense.Categories.Food, 15.90M);
+            new Expense("Movie tickets", Expense.Categories.Entertainment, 250.00M);
+
+            Expense.Categories categoryOther = Expense.Categories.Other;
+            decimal sumOther = Expense.SumExpenses(Expense.TotalExpenses, categoryOther);
+            Assert.AreEqual(13398.90M, sumOther);
+
+            Expense.Categories categoryFood = Expense.Categories.Food;
+            decimal sumFood = Expense.SumExpenses(Expense.TotalExpenses, categoryFood);
+            Assert.AreEqual(65.80M, sumFood);
+
+            Expense.Categories categoryEntertainment = Expense.Categories.Entertainment;
+            decimal sumEntertainment = Expense.SumExpenses(Expense.TotalExpenses, categoryEntertainment);
+            Assert.AreEqual(15150M, sumEntertainment);
+
+            decimal sum = Expense.SumExpenses(Expense.TotalExpenses);
+            Assert.AreEqual(28614.70M, sum);
+
+            Expense.TotalExpenses.Clear();
         }
     }
 }
